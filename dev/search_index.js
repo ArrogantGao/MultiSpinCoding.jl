8ca445bdc43f43be8ecbf5a33f5ff1caefff3da9
Documenter.jl <documenter@juliadocs.github.io>
2025-06-22 11:12:00 +0000

    build based on 2c78e8b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MultiSpinCoding","page":"Home","title":"MultiSpinCoding","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultiSpinCoding.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
